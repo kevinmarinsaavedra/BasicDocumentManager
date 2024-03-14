3242296a2140f7e77704393b449ed25720e528ba
Kevin Marin <kevinmarinsaavedra@gmail.com>
2024-03-14 14:16:46 +0100

    Initial Commit

---
+++ BasicDocumentManager/BasicDocumentManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicDocumentManagerApp.swift
+//  BasicDocumentManager
+//
+//  Created by Kevin Marin on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicDocumentManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicDocumentManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicDocumentManager
+//
+//  Created by Kevin Marin on 14/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
